f7c7ba20b57c8e7e9906671579252bc4d63d4816
cccrespo19 <140754478+cccrespo19@users.noreply.github.com>
2023-08-08 11:01:19 -0500

    Created function getComputerChoice that returns random "Rock", "Paper", "Scissors"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log("Hello!");
+
+function getRandomInt(int) {
+    // Creates random integer from 0 to (int - 1) for a total of int numbers
+    return Math.floor(Math.random() * int);
+}
+
+function getComputerChoice() {
+    const randomInt = getRandomInt(3);
+    if (randomInt === 0) {
+        return "Rock";
+    }
+    else if (randomInt === 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
